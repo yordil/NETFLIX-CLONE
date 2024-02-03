58f2bb1c5276c69f0a5633120ae0080883efb407
yordanos legesse <yordanoslegesse15@gmail.com>
2024-02-03 17:33:10 +0300

    Row added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,12 +2,13 @@ import React from "react";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import Banner from "../../Components/Banner/Banner";
-
+import RowList from "../../Components/Rows/RowList/RowList";
 const Home = () => {
 	return (
 		<>
 			<Header />
 			<Banner/>
+			<RowList />
 			<Footer />
 		</>
 	);

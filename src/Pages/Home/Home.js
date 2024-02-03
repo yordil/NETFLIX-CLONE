5c34c3e415318448d04bd53bdad7d0a75306ab9c
yordanos legesse <yordanoslegesse15@gmail.com>
2024-02-03 14:54:25 +0300

    Banner Added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
+import Banner from "../../Components/Banner/Banner";
 
 const Home = () => {
 	return (
 		<>
 			<Header />
+			<Banner/>
 			<Footer />
 		</>
 	);
